9574702ebc89ff238034b64d6e13ad529ca01517
Marco Rossel <andresmarcorossel@gmail.com>
2023-11-08 15:48:19 -0300

    encryptando password

--- controllers/crear-usuario.js
+++ controllers/crear-usuario.js
@@ -1,11 +1,18 @@
 const NuevoUsuario = require('../models/modelo-usuario.js');
+const bcrypt = require('bcryptjs');
 
 const registrarUsuario = async (req, res) =>{
   try {
     const {nombre, email, password} = req.body; 
     console.log('req body: ', nombre, email, password);
     // antes de guardar necesitamos enctyptar la contrase;a
-    const usuarioNuevo = await new NuevoUsuario(req.body)
+    const passwordCrypt = bcrypt.hashSync(password, bcrypt.genSaltSync(10))
+    const obj = {
+      nombre: nombre,
+      email: email,
+      password: passwordCrypt
+    }
+    const usuarioNuevo = await new NuevoUsuario(obj)
     await usuarioNuevo.save();
 
     if (!usuarioNuevo) {
@@ -13,6 +20,9 @@ const registrarUsuario = async (req, res) =>{
     }
     res.status(201).json(usuarioNuevo)
   } catch (error) {
+    console.log(error);
+    if (error.code === 11000) return res.status(400).json({mensajeError: 'El correo ya se encuentra registrado!'})
+    // error 11000 testeado correctamente.
     res.status(400).send(`error en registrar Usuario: ${error}`);
   }
 }
@@ -24,7 +34,16 @@ const registrarUsuario = async (req, res) =>{
     "password": "qwe",
     "_id": "654bd45848b4e6b239da7df3",
     "__v": 0
-} */
+
+    nuevo output con password encryptado:
+    {
+    "nombre": "marco",
+    "email": "example@gmail.com",
+    "password": "$2a$10$h705v.XUd07CwWgppGOQ7.Sd0NyzjVCNA/6kzxVxvCy6ZWrRdfwBu",
+    "_id": "654bd7aad86f68328fdd8820",
+    "__v": 0
+}
+  } */
 
 module.exports = {
   registrarUsuario
