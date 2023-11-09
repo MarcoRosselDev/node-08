be9b76f46c15619664c1d3206df5c4b11ca47516
Marco Rossel <andresmarcorossel@gmail.com>
2023-11-09 17:17:19 -0300

    login registrar fn

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const connect = require('./db.js');
 const path = require('path');
 
 
-const {registrarUsuario, loginUsuario} = require('./controllers/crear-usuario.js')
+const {registrarUsuario, loginUsuario} = require('./controllers/crear-usuario.js');
+const {getTarea, postTarea} = require('./controllers/crear-tareas.js')
 
 // pages
 app.use(express.json())
@@ -13,10 +14,12 @@ app.use('/login', express.static(path.join(__dirname, 'pages', 'login')))
 app.use('/registrar', express.static(path.join(__dirname, 'pages', 'registrar')))
 app.use('/tareas', express.static(path.join(__dirname, 'pages', 'tareas')))
 
-// api
-app.get('/registrar', registrarUsuario)
-app.post('/login', loginUsuario)
-
+// api usuario
+app.get('/api/registrar', registrarUsuario)
+app.post('/api/login', loginUsuario)
+// api tareas
+app.post('/api/tarea', postTarea)
+app.get('/api/tarea', getTarea)
 
 
 
---
+++ controllers/crear-tareas.js
@@ -0,0 +1,22 @@
+const Tarea = require('../models/modelo-tarea.js');
+
+const postTarea = async (req, res) =>{
+  try {
+    console.log('post tarea');
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+const getTarea = async (req, res) =>{
+  try {
+    console.log('get Tarea');
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+module.exports = {
+  postTarea,
+  getTarea
+}
