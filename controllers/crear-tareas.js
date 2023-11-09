b765eef0bcb751fc02cfc365119451199c1a46a7
Marco Rossel <andresmarcorossel@gmail.com>
2023-11-09 18:59:30 -0300

    splice jwt from header

--- controllers/crear-tareas.js
+++ controllers/crear-tareas.js
@@ -2,8 +2,13 @@ const Tarea = require('../models/modelo-tarea.js');
 
 const postTarea = async (req, res) =>{
   try {
-    console.log(req.authorizartion);
-    console.log('post tarea');
+    //console.log(req);
+    const bearer = req.rawHeaders[1];
+    const jwt = bearer.split(' ')[1];
+    console.log(jwt);
+    //console.log(req.header.param);
+    //console.log(req.header);
+    res.json({msj:'data'})
   } catch (error) {
     console.log(error);
   }
