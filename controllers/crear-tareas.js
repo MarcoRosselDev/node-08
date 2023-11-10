56e1af8e13ee77eff48ba87faa47f81425c3aa33
Marco Rossel <andresmarcorossel@gmail.com>
2023-11-09 23:06:35 -0300

    jwt para crear tareas

--- controllers/crear-tareas.js
+++ controllers/crear-tareas.js
@@ -5,26 +5,24 @@ const key_jwt = process.env.JWT_KEY;
 
 const postTarea = async (req, res) =>{
   try {
-    //console.log(req);
+    // sacar jwt del header
     const bearer = req.rawHeaders[1];
-    //const jwt_header = bearer.split(' ')[1];
-    const jwt_header = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'
-    console.log('jwt from header :', jwt_header);
-    try {
-      const comp_jwt = jwt.verify(jwt_header, key_jwt);
-      return comp_jwt;
-    } catch (error) {
-      res.status(400).json({msj: 'token invalido', err: error})
-    }
-    //console.log('boolean de comparacion: ', comp_jwt);
-    //console.log(req.header.param);
-    //console.log(req.header);
-    if (comp_jwt === undefined) {
-      return res.status(400).json({data: 'laskldjf'})
-    }
-    res.json({msj:comp_jwt})
+    const jwt_header = bearer.split(' ')[1];
+    // verificar y sacar la info del jwt
+    const jwt_info = jwt.verify(jwt_header, key_jwt);
+    const {nombre:nombre_jwt, id:id_jwt} = jwt_info;
+    // crear tarea con esta info
+    const nuevaTarea = await new Tarea({
+      user_id: id_jwt,
+      user_name: nombre_jwt,
+      titulo: req.body.titulo,
+      contenido: req.body.contenido
+    });
+    await nuevaTarea.save();
+
+    res.status(201).json({msj: nuevaTarea})
   } catch (error) {
-    console.log(error);
+    res.status(401).json({errorMesaje: `${error}`})
   }
 }
 
--- models/modelo-tarea.js
+++ models/modelo-tarea.js
@@ -1,17 +1,27 @@
 const mongoose = require('mongoose');
 
+/* 
+user_id: id_jwt,
+      user_name: nombre_jwt,
+      titulo: req.body.titulo,
+      contenido: req.body.contenido */
+
 const tarea = new mongoose.Schema({
   user_id: {
     type: String,
     require: true
   },
-  contenido:{
+  user_name: {
     type: String,
-    default: 'contenido por defercto',
+    require: true
   },
   titulo:{
     type: String,
     default: 'titulo por defecto'
+  },
+  contenido:{
+    type: String,
+    default: 'contenido por defercto',
   }
 },
 {timestamps: true})
