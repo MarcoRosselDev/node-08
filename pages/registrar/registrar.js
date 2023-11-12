75a4b4df942ffb654fb1147f72720b8245cb1314
Marco Rossel <andresmarcorossel@gmail.com>
2023-11-11 22:58:48 -0300

    registrar vanilla 03

--- pages/registrar/registrar.js
+++ pages/registrar/registrar.js
@@ -4,10 +4,27 @@ const password = document.getElementById('password');
 const password_rep = document.getElementById('password-rep');
 const enviar = document.querySelector('.enviar')
 
-enviar.addEventListener('click', function (e) {
+enviar.addEventListener('click', async function (e) {
   e.preventDefault();
   console.log('nombre : ', nombre.value);
   console.log('email : ', email.value);
   console.log('password : ', password.value);
   console.log('password_rep : ', password_rep.value);
+
+  if (password.value === password_rep.value) {
+    try {
+      const respuesta = await fetch('/api/registrar', {
+        method: "POST",
+        body: JSON.stringify({
+          nombre: nombre.value,
+          email: email.value,
+          password: password.value
+        })
+      })
+      respuesta.then(resp => resp.json())
+        .then(data => console.log(data))
+    } catch (error) {
+      console.log(error);
+    }
+  }
 })
