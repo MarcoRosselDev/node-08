03fa13a3472eacc90d81320c6ebae5a8000f657f
Marco Rossel <andresmarcorossel@gmail.com>
2023-11-12 06:00:04 -0300

    registrar vanilla 09

--- pages/registrar/add.js
+++ pages/registrar/add.js
@@ -1,1 +1,23 @@
-console.log('add');
+const nombre = document.getElementById('nombre');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password_rep = document.getElementById('password-rep');
+const enviar = document.querySelector('.enviar')
+const alert = document.querySelector('.alert');
+
+enviar.addEventListener('click', function (e) {
+  e.preventDefault();
+  console.log(alert.classList);
+  alert.classList.add('mistake')
+  alert.innerText = 'Error de consola'
+  console.log(alert.innerText);
+
+  setTimeout(()=>{
+    alert.classList.remove('mistake');
+    alert.innerText = '';
+    // aplicar un boton para ir a login
+    //user esto con el login...
+    //window.location = 'http://localhost:5500/pages/tareas/'
+  },2000)
+
+})
