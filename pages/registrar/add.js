d4695d99995ab001920ed3d9b75d228a77cbb56f
Marco Rossel <andresmarcorossel@gmail.com>
2023-11-13 02:07:55 -0300

    registrar vanilla 16

--- pages/registrar/add.js
+++ pages/registrar/add.js
@@ -7,10 +7,6 @@ const msjContenedor = document.querySelector('.msj-contenedor');
 
 enviar.addEventListener('click', function (e) {
   e.preventDefault();
-  /* console.log(alert.classList);
-  alert.classList.add('good')
-  alert.innerText = 'Cuenta creada'
-  console.log(alert.innerText); */
   msjContenedor.classList.add('good')
   msjContenedor.innerHTML = `
   <p>usuario creado, <a href="http://127.0.0.1:5500/pages/login/index.html">ir a login</a></p>
@@ -27,5 +23,35 @@ enviar.addEventListener('click', function (e) {
     msjContenedor.removeChild(this.parentNode.parentNode.children[0]);
     this.parentNode.parentNode.classList.remove('good')
     this.parentElement.children[0].classList.add('hiden');
+  })
+})
+
+enviar.addEventListener('click', function (e) {
+  e.preventDefault();
+  msjContenedor.classList.add('mistake')
+  msjContenedor.innerHTML = `
+  <p>Error 001, los passwords no coinsiden</p>
+  <div>
+    <button class="x">X</button>
+  </div>
+  `;
+  const x = document.querySelector('.x');
+  try {
+    setTimeout(()=>{
+      console.log('borrado automatico');
+      x.parentNode.parentNode.classList.remove('mistake')
+      x.parentElement.children[0].classList.add('hiden');
+      msjContenedor.removeChild(x.parentNode.parentNode.children[0]);
+    }, 4000)
+  } catch (error) {
+    console.log(error);
+  }
+  x.addEventListener('click', function (e) {
+    e.preventDefault();
+    console.log('x clicked');
+
+    this.parentNode.parentNode.classList.remove('mistake')
+    this.parentElement.children[0].classList.add('hiden');
+    msjContenedor.removeChild(this.parentNode.parentNode.children[0]);
   })
 })
--- pages/registrar/registrar.js
+++ pages/registrar/registrar.js
@@ -3,7 +3,7 @@ const email = document.getElementById('email');
 const password = document.getElementById('password');
 const password_rep = document.getElementById('password-rep');
 const enviar = document.querySelector('.enviar')
-const alert = document.querySelector('.alert');
+const msjContenedor = document.querySelector('.msj-contenedor');
 
 enviar.addEventListener('click', async function (e) {
   e.preventDefault();
@@ -29,10 +29,32 @@ enviar.addEventListener('click', async function (e) {
           password.value = '';
           password_rep.value = '';
           // add msj
-          setTimeout(()=>{
-            // cargar mensaje en div = successufl
-            // eliminarlo autmaticamente
-          }, 5000)
+          msjContenedor.classList.add('good')
+          msjContenedor.innerHTML = `
+          <p>usuario creado, <a href="http://127.0.0.1:5500/pages/login/index.html">ir a login</a></p>
+          <div>
+            <button class="x">X</button>
+          </div>
+          `;
+          const x = document.querySelector('.x');
+          try {
+            setTimeout(()=>{
+              console.log('borrado automatico');
+              x.parentNode.parentNode.classList.remove('good')
+              x.parentElement.children[0].classList.add('hiden');
+              msjContenedor.removeChild(x.parentNode.parentNode.children[0]);
+            }, 4000)
+          } catch (error) {
+            console.log(error);
+          }
+          x.addEventListener('click', function (e) {
+            e.preventDefault();
+            console.log('x clicked');
+        
+            this.parentNode.parentNode.classList.remove('good')
+            this.parentElement.children[0].classList.add('hiden');
+            msjContenedor.removeChild(this.parentNode.parentNode.children[0]);
+          })
           console.log(data);
           return 1234
         })
@@ -49,6 +71,36 @@ enviar.addEventListener('click', async function (e) {
       console.log(error);
     }
   } else{
+    //---
+
+    msjContenedor.classList.add('mistake')
+    msjContenedor.innerHTML = `
+    <p>Error 001, los passwords no coinsiden</p>
+    <div>
+      <button class="x">X</button>
+    </div>
+    `;
+    const x = document.querySelector('.x');
+    try {
+      setTimeout(()=>{
+        console.log('borrado automatico');
+        x.parentNode.parentNode.classList.remove('mistake')
+        x.parentElement.children[0].classList.add('hiden');
+        msjContenedor.removeChild(x.parentNode.parentNode.children[0]);
+      }, 4000)
+    } catch (error) {
+      console.log(error);
+    }
+    x.addEventListener('click', function (e) {
+      e.preventDefault();
+      console.log('x clicked');
+  
+      this.parentNode.parentNode.classList.remove('mistake')
+      this.parentElement.children[0].classList.add('hiden');
+      msjContenedor.removeChild(this.parentNode.parentNode.children[0]);
+    })
+
+    //---
     // appEnd msj en el div alert
     console.log("los passwords no coinsiden");
   }
