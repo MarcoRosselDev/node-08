ccc2d312577dd1e9587bb0e2ec7d163778425742
Marco Rossel <andresmarcorossel@gmail.com>
2023-11-12 23:01:21 -0300

    registrar vanilla 14

--- pages/registrar/add.js
+++ pages/registrar/add.js
@@ -3,8 +3,7 @@ const email = document.getElementById('email');
 const password = document.getElementById('password');
 const password_rep = document.getElementById('password-rep');
 const enviar = document.querySelector('.enviar')
-const alert = document.querySelector('.alert');
-const x = document.querySelector('.x');
+const msjContenedor = document.querySelector('.msj-contenedor');
 
 enviar.addEventListener('click', function (e) {
   e.preventDefault();
@@ -12,6 +11,23 @@ enviar.addEventListener('click', function (e) {
   alert.classList.add('good')
   alert.innerText = 'Cuenta creada'
   console.log(alert.innerText); */
+  console.log(msjContenedor.classList);
+  msjContenedor.classList.add('good')
+  msjContenedor.innerHTML = `
+  <p>usuario creado, <a href="http://127.0.0.1:5500/pages/login/index.html">ir a login</a></p>
+  <div>
+    <button class="x">X</button>
+  </div>
+  `;
+  const x = document.querySelector('.x');
+  
+  x.addEventListener('click', function (e) {
+    e.preventDefault();
+    console.log('x clicked');
 
+    console.log(this.parentNode.parentNode.children[0]);
+    console.log(this.parentNode.parentNode.children[1]);
 
+    //console.log(this.parentNode.parentNode.classList.remove('good'));
+  })
 })
