7ec6598706a0fcbc25c45985f519f4c0789d7b8a
Marco Rossel <andresmarcorossel@gmail.com>
2023-11-14 07:24:58 -0300

    login vanilla 12

--- pages/tareas/error.js
+++ pages/tareas/error.js
@@ -1,13 +1,20 @@
 const click = document.getElementById('click');
+const titulo = document.getElementById('titulo');
+const contenido = document.getElementById('contenido');
 const main = document.querySelector('main');
+const guardar = document.getElementById('tareaBTN')
 
-click.addEventListener('click', function (e) {
+guardar.addEventListener('click', function (e) {
   e.preventDefault();
-  console.log(main.children);
-  main.innerHTML = `
-  <div class="contenedor">
-    <p>No estas logeado!</p>
-    <a href="http://localhost:3000/login">ir a login</a>
-  </div>
+  console.log(titulo.value);
+  console.log(contenido.value);
+  const containTask = this.parentElement.parentNode.children[1];
+  const div = document.createElement('div');
+  div.classList.add('single-list')
+  div.innerHTML = `
+  <p>${titulo.value}</p>
+  <p>${contenido.value}</p>
   `
+
+  containTask.appendChild(div)
 })
