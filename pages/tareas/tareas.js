49c8f71c553121cfbefb66482a3ef0ff7104f495
Marco Rossel <andresmarcorossel@gmail.com>
2023-11-15 21:58:57 -0300

    login vanilla error solved | 01

--- pages/tareas/tareas.js
+++ pages/tareas/tareas.js
@@ -3,6 +3,8 @@ const header = document.querySelector('header');
 
 const cargarCookie = async () =>{
   try {
+    // tratamos de obtener la cookie de el navegador si es que exciste,
+    // si no le mostramos un mensaje de logearse pls
     const getCookie = await fetch('/api/get-cookie', {
       method: 'GET',
       headers: {
@@ -10,9 +12,9 @@ const cargarCookie = async () =>{
         // 'Content-Type': 'application/x-www-form-urlencoded',
       }
     })
-
-    console.log(getCookie, 'get cookie log');
     if (getCookie.status === 200) {
+      // entes de procesar el then manipulamos el dom para mostrar 
+      // el formulario para una nueva tarea
       main.innerHTML = `
       <div class="contenedor">
         <div class="form-tarea">
@@ -27,20 +29,22 @@ const cargarCookie = async () =>{
         </div>
       </div>
       `;
-      // desde aqui se pueden cargar los elementos para manipular
       const prom = getCookie.json();
       prom.then(async (data) => {
-        // para innerHtml dom manipulation
-        const tituloV = document.getElementById('titulo');
-        const contenidoV = document.getElementById('contenido');
-        const contenedorTareas = document.querySelector('.contenedor-tareas');
-        const guardar = document.getElementById('tareaBTN');
-        const jwtCookie = data.cookie.jwt;
+        // desde aqui se pueden cargar los elementos para manipular
+        const tituloV = document.getElementById('titulo'); // input
+        const contenidoV = document.getElementById('contenido'); // input
+        const guardar = document.getElementById('tareaBTN'); // boton guardar tarea
+        const contenedorTareas = document.querySelector('.contenedor-tareas'); // manipular DOM
+        const jwtCookie = data.cookie.jwt; // jwt sacada de la cookie
 
         guardar.addEventListener('click',async function (e) {
           e.preventDefault();
           try {
 
+            // post nueva tarea
+            // solo enviamos el titulo y el contenido
+            // la id del usuario y el nombre de usuario se extraen de el jwt que se saco de la cookie
             const resp = await fetch('/api/tarea', {
               method: 'POST',
               headers: {
@@ -53,13 +57,12 @@ const cargarCookie = async () =>{
                 contenido: contenidoV.value
               })
             })
-            console.log(resp);
             if (resp.status === 201) {
               const promess = resp.json();
+              // data --> array de tareas
               promess.then((data) =>{
-                //const contenedorTareas = document.querySelector('contenedor-tareas');
-                console.log(data);
-                // print dom data
+                // la data contiene el user_id y _id de la tarea
+                // enviada desde el post que estrajo la user_id del jwt cookie decode
                 const div = document.createElement('div');
                 div.classList.add('single-list')
                 div.innerHTML = `
@@ -68,27 +71,38 @@ const cargarCookie = async () =>{
                 <p class="hiden">${data.user_id}</p>
                 <p class="hiden">${data._id}</p>
                 `;
-                contenedorTareas.appendChild(div);
+                // ocultamos el id de usuario e id para la manipulacion de estas tareas
+                contenedorTareas.appendChild(div); // lo agregamos al final del contenedor de tareas
               })
             } else{
+              // posdriamos imprimir algo en el dom en el futuro
               console.log('no se guardo la tarea correctamente');
             }
           } catch (error) {
+            // algun error en el fetch post tarea
             console.log(error);
           }
         })
 
-        console.log('data ---> ', data);
+        // ejemplo de lo que devuelve data, que viene de getCookie fn
+        /* data = {  
+            cookie: {jwt: 'asdfa...'},
+            id: '123123hjk23412341llk...',
+            nombre: 'lushito'} */
+
         //const jwt = data.cookie.jwt;
+
         const id = data.id;
         const nombre = data.nombre
 
+        // saludamos al usuario ...
         header.innerHTML = `
           <p>header</p>
           <p>Welcome ${nombre}</p>
           <p>logout</p>`;
-
-        //console.log(jwt);
+        
+        // con toda la info disponible podemos realisar un fetch GET
+        // para obtener todas las tareas guardadas de el usuario x
         const getTarea = await fetch(`/api/tarea/${id}`, {
           method: 'GET',
           headers:{
@@ -96,13 +110,19 @@ const cargarCookie = async () =>{
             'Content-Type': 'application/json'
           }
         })
-        console.log(getTarea);
         if (getTarea.status === 200) {
           const proms = getTarea.json();
           proms.then(data => {
-            console.log('we are in get tareas', contenedorTareas);
+            // la data es un objeto con todas las tareas,
+            // si no tiene devolvera un arreglo vacio ====> []
+            // ejemplo:
+            /* 
+            [{_id: '...', user_id: '...', user_name: '...', titulo: '...', contenido: '...'},
+            {_id: '...', user_id: '...', user_name: '...', titulo: '...', contenido: '...'},
+            {_id: '...', user_id: '...', user_name: '...', titulo: '...', contenido: '...'}] */
+
+            // iteramos por el arreglo con map y manipulamos el dom con la lista de tareas
             let print = "";
-            console.log('data:--->', data);
             data.map(i => {
               print += `
               <div class="single-list">
@@ -115,10 +135,13 @@ const cargarCookie = async () =>{
             contenedorTareas.innerHTML = print;
           })
         } else {
-          console.log('');
+          // podriamos trabajar como elnviar un msj al user de el error al cargar las tareas
+          console.log('algun error en la obtencion de las tareas');
         }
       })
     } else {
+      // si no esta logeado le decimos que valla a logearse
+      // esto despues de la peticion obtener cookie del navegador
       main.innerHTML = `
       <div class="contenedor">
         <p>No estas logeado!</p>
@@ -132,11 +155,3 @@ const cargarCookie = async () =>{
 }
 
 cargarCookie();
-
-// extraer la informacion de la cookie para saludar
-// hola... username
-
-// cargar input y button de nueva tarea
-
-// realizar un fetch GET  de las tareas del usuario
-// cargar en el dom
