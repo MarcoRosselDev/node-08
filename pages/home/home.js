ef6d9a506db52777f68fd26de0124419592abe22
Marco Rossel <andresmarcorossel@gmail.com>
2023-11-18 06:44:55 -0300

    home | 03

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,13 +1,17 @@
-const togg = document.querySelector('.toggle');
+const toggleBtn = document.querySelector('.toogleBtn');
 const navList = document.querySelector('.navList');
+const styleMode = document.querySelector('.styleMode');
 
-console.log(togg);
+styleMode.addEventListener('click', function (e) {
+  e.preventDefault();
+  // crear una cookie con el key=style, value=dark|light 
+})
 
-/* toggle.addEventListener('click', function (e) {
+toggleBtn.addEventListener('click', function (e) {
   e.preventDefault();
   if (navList.classList.contains('hide')) {
     navList.classList.remove('hide')
   } else{
     navList.classList.add('hide')
   }
-}) */
+})
