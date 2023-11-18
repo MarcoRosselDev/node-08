5d87a136f5000073baaa0f715be6c7c06ba9a94d
Marco Rossel <andresmarcorossel@gmail.com>
2023-11-18 05:31:46 -0300

    home | 02

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,1 +1,13 @@
-console.log('home');
+const togg = document.querySelector('.toggle');
+const navList = document.querySelector('.navList');
+
+console.log(togg);
+
+/* toggle.addEventListener('click', function (e) {
+  e.preventDefault();
+  if (navList.classList.contains('hide')) {
+    navList.classList.remove('hide')
+  } else{
+    navList.classList.add('hide')
+  }
+}) */
