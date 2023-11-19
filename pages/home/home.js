7066e515c7fe5f1e6ede0b181a741286d13cc571
Marco Rossel <andresmarcorossel@gmail.com>
2023-11-19 12:24:28 -0300

    home | 11

--- pages/home/home.js
+++ pages/home/home.js
@@ -2,18 +2,20 @@ const toggleBtn = document.querySelector('.toogleBtn');
 const divNav = document.querySelector('.divNav');
 const styleMode = document.querySelector('.styleMode');
 const lightMode = document.querySelector('.light-mode');
-const main = document.querySelector('main');
+const over = document.querySelector('.over')
 
 toggleBtn.addEventListener('click', function (e) {
   e.preventDefault();
   if (divNav.classList.contains('hide')) {
     divNav.classList.remove('hide');
+    over.classList.add('over-lide');
     divNav.classList.add('navList');
-    main.classList.add('another')
+    //main.classList.add('another');
   } else{
     divNav.classList.add('hide');
     divNav.classList.remove('navList');
-    main.classList.remove('another')
+    over.classList.remove('over-lide');
+    //main.classList.remove('another')
   }
 })
 
@@ -22,4 +24,12 @@ lightMode.addEventListener('click', function (e) {
   console.log('you are clicked light mode');
   // ahora crear una cookie con la clave valor:
   // mode = night|light
+})
+
+over.addEventListener('click', function (e) {
+  e.preventDefault();
+  divNav.classList.remove('navList')
+  divNav.classList.add('hide');
+  over.classList.remove('over-lide')
+  console.log('click main');
 })
