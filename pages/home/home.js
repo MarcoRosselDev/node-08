6f16eeb3c6bb1d33dda7894e56da86eca69bf209
Marco Rossel <andresmarcorossel@gmail.com>
2023-11-19 10:09:03 -0300

    home | 10

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,23 +1,25 @@
 const toggleBtn = document.querySelector('.toogleBtn');
 const divNav = document.querySelector('.divNav');
 const styleMode = document.querySelector('.styleMode');
-
-console.log(styleMode);
-styleMode.addEventListener('click', function (e) {
-  e.preventDefault();
-  console.log('light mode');
-  console.log(this.parentElement);
-  // crear una cookie con el key=style, value=dark|light 
-})
+const lightMode = document.querySelector('.light-mode');
+const main = document.querySelector('main');
 
 toggleBtn.addEventListener('click', function (e) {
   e.preventDefault();
   if (divNav.classList.contains('hide')) {
     divNav.classList.remove('hide');
     divNav.classList.add('navList');
+    main.classList.add('another')
   } else{
     divNav.classList.add('hide');
     divNav.classList.remove('navList');
+    main.classList.remove('another')
   }
 })
 
+lightMode.addEventListener('click', function (e) {
+  e.preventDefault();
+  console.log('you are clicked light mode');
+  // ahora crear una cookie con la clave valor:
+  // mode = night|light
+})
