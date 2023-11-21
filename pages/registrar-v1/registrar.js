cd34a27ee2891ac92f9fca543b087bbc1b7e7298
Marco Rossel <andresmarcorossel@gmail.com>
2023-11-21 14:20:41 -0300

    registarar | new version

---
+++ pages/registrar-v1/registrar.js
@@ -0,0 +1,107 @@
+const nombre = document.getElementById('nombre');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password_rep = document.getElementById('password-rep');
+const enviar = document.querySelector('.enviar')
+const msjContenedor = document.querySelector('.msj-contenedor');
+
+enviar.addEventListener('click', async function (e) {
+  e.preventDefault();
+  if (password.value === password_rep.value) {
+    try {
+      const respuesta = await fetch('/api/registrar', {
+        method: "POST",
+        headers: {
+          'Content-Type': 'application/json'
+          // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: JSON.stringify({
+          nombre: nombre.value,
+          email: email.value,
+          password: password.value
+        })
+      })
+      if (respuesta.status === 201) {
+        const prom = respuesta.json();
+        prom.then(data =>{
+          nombre.value = '';
+          email.value = '';
+          password.value = '';
+          password_rep.value = '';
+          // add msj
+          msjContenedor.classList.add('good')
+          msjContenedor.innerHTML = `
+          <p>usuario creado, <a href="http://127.0.0.1:5500/pages/login/index.html">ir a login</a></p>
+          <div>
+            <button class="x">X</button>
+          </div>
+          `;
+          const x = document.querySelector('.x');
+          try {
+            setTimeout(()=>{
+              console.log('borrado automatico');
+              x.parentNode.parentNode.classList.remove('good')
+              x.parentElement.children[0].classList.add('hiden');
+              msjContenedor.removeChild(x.parentNode.parentNode.children[0]);
+            }, 4000)
+          } catch (error) {
+            console.log(error);
+          }
+          x.addEventListener('click', function (e) {
+            e.preventDefault();
+            console.log('x clicked');
+        
+            this.parentNode.parentNode.classList.remove('good')
+            this.parentElement.children[0].classList.add('hiden');
+            msjContenedor.removeChild(this.parentNode.parentNode.children[0]);
+          })
+          console.log(data);
+          return 1234
+        })
+        .then((data) =>{
+          console.log('data from then: ', data);
+        })
+      }
+      // div alert msj
+      console.log('status from respuesta : ',respuesta.status);
+      //await respuesta.json();
+      //console.log(respuesta.json());
+      //return respuesta.json()
+    } catch (error) {
+      console.log(error);
+    }
+  } else{
+    //---
+
+    msjContenedor.classList.add('mistake')
+    msjContenedor.innerHTML = `
+    <p>Error 001, los passwords no coinsiden</p>
+    <div>
+      <button class="x">X</button>
+    </div>
+    `;
+    const x = document.querySelector('.x');
+    try {
+      setTimeout(()=>{
+        console.log('borrado automatico');
+        x.parentNode.parentNode.classList.remove('mistake')
+        x.parentElement.children[0].classList.add('hiden');
+        msjContenedor.removeChild(x.parentNode.parentNode.children[0]);
+      }, 4000)
+    } catch (error) {
+      console.log(error);
+    }
+    x.addEventListener('click', function (e) {
+      e.preventDefault();
+      console.log('x clicked');
+  
+      this.parentNode.parentNode.classList.remove('mistake')
+      this.parentElement.children[0].classList.add('hiden');
+      msjContenedor.removeChild(this.parentNode.parentNode.children[0]);
+    })
+
+    //---
+    // appEnd msj en el div alert
+    console.log("los passwords no coinsiden");
+  }
+})
--- pages/registrar/registrar.js
+++ pages/registrar/registrar.js
@@ -1,107 +1,26 @@
-const nombre = document.getElementById('nombre');
-const email = document.getElementById('email');
-const password = document.getElementById('password');
-const password_rep = document.getElementById('password-rep');
-const enviar = document.querySelector('.enviar')
-const msjContenedor = document.querySelector('.msj-contenedor');
+const toggleBtn = document.querySelector('.toogleBtn');
+const divNav = document.querySelector('.divNav');
+const over = document.querySelector('.over');
+//Esme_KD
 
-enviar.addEventListener('click', async function (e) {
+toggleBtn.addEventListener('click', function (e) {
   e.preventDefault();
-  if (password.value === password_rep.value) {
-    try {
-      const respuesta = await fetch('/api/registrar', {
-        method: "POST",
-        headers: {
-          'Content-Type': 'application/json'
-          // 'Content-Type': 'application/x-www-form-urlencoded',
-        },
-        body: JSON.stringify({
-          nombre: nombre.value,
-          email: email.value,
-          password: password.value
-        })
-      })
-      if (respuesta.status === 201) {
-        const prom = respuesta.json();
-        prom.then(data =>{
-          nombre.value = '';
-          email.value = '';
-          password.value = '';
-          password_rep.value = '';
-          // add msj
-          msjContenedor.classList.add('good')
-          msjContenedor.innerHTML = `
-          <p>usuario creado, <a href="http://127.0.0.1:5500/pages/login/index.html">ir a login</a></p>
-          <div>
-            <button class="x">X</button>
-          </div>
-          `;
-          const x = document.querySelector('.x');
-          try {
-            setTimeout(()=>{
-              console.log('borrado automatico');
-              x.parentNode.parentNode.classList.remove('good')
-              x.parentElement.children[0].classList.add('hiden');
-              msjContenedor.removeChild(x.parentNode.parentNode.children[0]);
-            }, 4000)
-          } catch (error) {
-            console.log(error);
-          }
-          x.addEventListener('click', function (e) {
-            e.preventDefault();
-            console.log('x clicked');
-        
-            this.parentNode.parentNode.classList.remove('good')
-            this.parentElement.children[0].classList.add('hiden');
-            msjContenedor.removeChild(this.parentNode.parentNode.children[0]);
-          })
-          console.log(data);
-          return 1234
-        })
-        .then((data) =>{
-          console.log('data from then: ', data);
-        })
-      }
-      // div alert msj
-      console.log('status from respuesta : ',respuesta.status);
-      //await respuesta.json();
-      //console.log(respuesta.json());
-      //return respuesta.json()
-    } catch (error) {
-      console.log(error);
-    }
+  if (divNav.classList.contains('hide')) {
+    divNav.classList.remove('hide');
+    over.classList.add('over-lide');
+    divNav.classList.add('navList');
+    //main.classList.add('another');
   } else{
-    //---
-
-    msjContenedor.classList.add('mistake')
-    msjContenedor.innerHTML = `
-    <p>Error 001, los passwords no coinsiden</p>
-    <div>
-      <button class="x">X</button>
-    </div>
-    `;
-    const x = document.querySelector('.x');
-    try {
-      setTimeout(()=>{
-        console.log('borrado automatico');
-        x.parentNode.parentNode.classList.remove('mistake')
-        x.parentElement.children[0].classList.add('hiden');
-        msjContenedor.removeChild(x.parentNode.parentNode.children[0]);
-      }, 4000)
-    } catch (error) {
-      console.log(error);
-    }
-    x.addEventListener('click', function (e) {
-      e.preventDefault();
-      console.log('x clicked');
-  
-      this.parentNode.parentNode.classList.remove('mistake')
-      this.parentElement.children[0].classList.add('hiden');
-      msjContenedor.removeChild(this.parentNode.parentNode.children[0]);
-    })
-
-    //---
-    // appEnd msj en el div alert
-    console.log("los passwords no coinsiden");
+    divNav.classList.add('hide');
+    divNav.classList.remove('navList');
+    over.classList.remove('over-lide');
+    //main.classList.remove('another')
   }
+})
+
+over.addEventListener('click', function (e) {
+  e.preventDefault();
+  divNav.classList.remove('navList')
+  divNav.classList.add('hide');
+  over.classList.remove('over-lide')
 })
