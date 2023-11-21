b59f2f1a7d1942a1052d9c5f1131e4fd2a11b6e0
Marco Rossel <andresmarcorossel@gmail.com>
2023-11-21 15:14:35 -0300

    registarar | new version 3

---
+++ pages/registrar/obtenerStyleMode.js
@@ -0,0 +1,54 @@
+const head = document.querySelector('head');
+import {actualizarStyleMode} from './actualizarStyleMode.js';
+const div = document.querySelector('.divNav');
+const modeBtn = document.querySelector('.light-mode');
+
+const obtenerStyleMode = async () =>{
+  try {
+    const response = await fetch('/api/getStylecookie', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+      }
+    });
+    if (response.status === 200) {
+    const a = response.json();
+    a.then(data => {
+      head.children[3].href = `http://localhost:3000/registrar/registrar-${data.mode}.css`;
+      div.classList.add(data.mode);
+      if (data.mode === 'light') {
+        modeBtn.innerText = 'dark mode';
+      } else{
+        modeBtn.innerText = 'light mode';
+      }
+
+      modeBtn.addEventListener('click',function (e) {
+        e.preventDefault();
+        if (div.classList.contains('light') === true) {
+          div.classList.remove('light');
+          head.children[3].href = `http://localhost:3000/registrar/registrar-dark.css`;
+          div.classList.add('dark');
+          actualizarStyleMode('dark'); //<------------------------light or dark | aplicar toggle fn
+          modeBtn.innerText = 'light mode';
+        } else{
+          div.classList.remove('dark');
+          head.children[3].href = `http://localhost:3000/registrar/registrar-light.css`;
+          div.classList.add('light');
+          actualizarStyleMode('light');
+          modeBtn.innerText = 'dark mode';
+        }
+      })
+
+
+    })
+    } else{
+      console.log('no existe la cookie mode, la crearemos en dark por defecto para ti.');
+      actualizarStyleMode('dark');
+    }
+  } catch (error) {
+    console.log('error en tratar de obtener la cookie de mode si es que existe-->', error);
+  }
+}
+
+obtenerStyleMode();
