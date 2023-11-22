96adce7a6edcb5ac2fa988d558f851fafae3ca7d
Marco Rossel <andresmarcorossel@gmail.com>
2023-11-22 17:40:39 -0300

    registarar | transition practis 9

--- pages/transition/transition2.js
+++ pages/transition/transition2.js
@@ -1,10 +1,22 @@
 const toggleBtn = document.querySelector('.toggleBtn');
 const links = document.querySelectorAll('.links');
 const directorio = document.querySelector('.directorio');
+const estela = document.querySelector('.estela');
+const event = document.querySelector('.event');
+
 
 toggleBtn.addEventListener('click', function (e) {
   e.preventDefault();
 
   links.forEach(i => i.classList.toggle('linksTgl'));
-  directorio.classList.toggle('directorioTgl')
+  directorio.classList.toggle('directorioTgl');
+  estela.classList.toggle('estelaTgl');
+})
+
+event.addEventListener('click', function (e) {
+  e.preventDefault();
+  
+  links.forEach(i => i.classList.toggle('linksTgl'));
+  directorio.classList.toggle('directorioTgl');
+  estela.classList.toggle('estelaTgl');
 })
