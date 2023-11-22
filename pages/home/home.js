30193e678102b94c89a158fa78a0c6e6e7258901
Marco Rossel <andresmarcorossel@gmail.com>
2023-11-22 14:10:58 -0300

    registarar | transition practis 4

--- pages/home/home.js
+++ pages/home/home.js
@@ -3,24 +3,33 @@ const divNav = document.querySelector('.divNav');
 const over = document.querySelector('.over');
 //Esme_KD
 
-toggleBtn.addEventListener('click', function (e) {
+/* toggleBtn.addEventListener('click', function (e) {
   e.preventDefault();
   if (divNav.classList.contains('hide')) {
     divNav.classList.remove('hide');
     over.classList.add('over-lide');
     divNav.classList.add('navList');
+    divNav.classList.add('divNavToggle');
     //main.classList.add('another');
   } else{
     divNav.classList.add('hide');
     divNav.classList.remove('navList');
+    divNav.classList.remove('divNavToggle');
     over.classList.remove('over-lide');
     //main.classList.remove('another')
   }
+}) */
+
+
+toggleBtn.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  divNav.classList.toggle('divNavToggle')
+  over.classList.toggle('over-lide')
 })
 
 over.addEventListener('click', function (e) {
   e.preventDefault();
-  divNav.classList.remove('navList')
-  divNav.classList.add('hide');
+  divNav.classList.remove('divNavToggle')
   over.classList.remove('over-lide')
 })
