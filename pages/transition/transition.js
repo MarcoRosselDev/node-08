0d4ee467a1b7b3e3aff377bf21c07aaf9a75e0f2
Marco Rossel <andresmarcorossel@gmail.com>
2023-11-22 16:09:33 -0300

    registarar | transition practis 5

--- pages/transition/transition.js
+++ pages/transition/transition.js
@@ -1,8 +1,35 @@
-console.log('transition js test');
-const btn = document.querySelector('.btn');
-const div = document.querySelector('.contenedor')
+const toggleBtn = document.querySelector('.toogleBtn');
+const divNav = document.querySelector('.divNav');
+const over = document.querySelector('.over');
+//Esme_KD
 
-btn.addEventListener('click', function (e) {
+/* toggleBtn.addEventListener('click', function (e) {
   e.preventDefault();
-  div.classList.toggle('contenedorToggle')
+  if (divNav.classList.contains('hide')) {
+    divNav.classList.remove('hide');
+    over.classList.add('over-lide');
+    divNav.classList.add('navList');
+    divNav.classList.add('divNavToggle');
+    //main.classList.add('another');
+  } else{
+    divNav.classList.add('hide');
+    divNav.classList.remove('navList');
+    divNav.classList.remove('divNavToggle');
+    over.classList.remove('over-lide');
+    //main.classList.remove('another')
+  }
+}) */
+
+
+toggleBtn.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  divNav.classList.toggle('divNavToggle')
+  over.classList.toggle('over-lide')
+})
+
+over.addEventListener('click', function (e) {
+  e.preventDefault();
+  divNav.classList.remove('divNavToggle')
+  over.classList.remove('over-lide')
 })
