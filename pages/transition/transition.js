a6bc2d060caaf9db0b406ca137dc9104f6e99a71
Marco Rossel <andresmarcorossel@gmail.com>
2023-11-22 10:00:44 -0300

    registarar | transition practis 3

--- pages/transition/transition.js
+++ pages/transition/transition.js
@@ -1,19 +1,8 @@
 console.log('transition js test');
 const btn = document.querySelector('.btn');
 const div = document.querySelector('.contenedor')
-const hi = document.querySelector('.hi')
 
 btn.addEventListener('click', function (e) {
   e.preventDefault();
-  if (div.classList.contains('contenedor2')) {
-    div.classList.remove('contenedor2')
-  } else{
-    div.classList.add('contenedor2')
-  }
-
-})
-
-hi.addEventListener('click', function (e) {
-  e.preventDefault();
-  console.log('hi clicked');
+  div.classList.toggle('contenedorToggle')
 })
