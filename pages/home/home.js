e6be53645273e6a619e1fe38ff2143df2c433f53
Marco Rossel <andresmarcorossel@gmail.com>
2023-11-23 07:21:18 -0300

    registarar | transition practis 9 fixed 2

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,35 +1,22 @@
-const toggleBtn = document.querySelector('.toogleBtn');
-const divNav = document.querySelector('.divNav');
-const over = document.querySelector('.over');
-//Esme_KD
-
-/* toggleBtn.addEventListener('click', function (e) {
-  e.preventDefault();
-  if (divNav.classList.contains('hide')) {
-    divNav.classList.remove('hide');
-    over.classList.add('over-lide');
-    divNav.classList.add('navList');
-    divNav.classList.add('divNavToggle');
-    //main.classList.add('another');
-  } else{
-    divNav.classList.add('hide');
-    divNav.classList.remove('navList');
-    divNav.classList.remove('divNavToggle');
-    over.classList.remove('over-lide');
-    //main.classList.remove('another')
-  }
-}) */
+const toggleBtn = document.querySelector('.toggleBtn');
+const links = document.querySelectorAll('.links');
+const directorio = document.querySelector('.directorio');
+const estela = document.querySelector('.estela');
+const event = document.querySelector('.event');
 
 
 toggleBtn.addEventListener('click', function (e) {
   e.preventDefault();
 
-  divNav.classList.toggle('divNavToggle')
-  over.classList.toggle('over-lide')
+  links.forEach(i => i.classList.toggle('linksTgl'));
+  directorio.classList.toggle('directorioTgl');
+  estela.classList.toggle('estelaTgl');
 })
 
-over.addEventListener('click', function (e) {
+event.addEventListener('click', function (e) {
   e.preventDefault();
-  divNav.classList.remove('divNavToggle')
-  over.classList.remove('over-lide')
+  
+  links.forEach(i => i.classList.toggle('linksTgl'));
+  directorio.classList.toggle('directorioTgl');
+  estela.classList.toggle('estelaTgl');
 })
