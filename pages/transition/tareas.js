889e0dc7486dd7aaa90efb84aefa5aac27ebae41
Marco Rossel <andresmarcorossel@gmail.com>
2023-11-26 13:18:37 -0300

    login | tareas redesign 10

--- pages/transition/tareas.js
+++ pages/transition/tareas.js
@@ -1,78 +1,19 @@
 const toggleBtn = document.querySelector('.toggleBtn');
-const directorio = document.querySelector('.directorio');
-const links = document.querySelectorAll('.links');
-//const divEstela = document.querySelector('.divEstela');
-const evento = document.querySelector('.event');
-const estela = document.querySelector('.estela');
-
-
-toggleBtn.addEventListener('click', function (e) {
-  e.preventDefault();
-  directorio.classList.toggle('directorioTgl');
-  links.forEach(i => i.classList.toggle('linksTgl'));
-  estela.classList.toggle('estelaTgl');
-})
-
-evento.addEventListener('click', function (e) {
-  e.preventDefault();
-  console.log('click en estela');
-  estela.classList.toggle('estelaTgl');
-  directorio.classList.toggle('directorioTgl')
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* const toggleBtn = document.querySelector('.toggleBtn');
 const links = document.querySelectorAll('.links');
 const directorio = document.querySelector('.directorio');
 const estela = document.querySelector('.estela');
-const estealMain = document.querySelector('.estela-main');
+const event = document.querySelector('.event');
 
 toggleBtn.addEventListener('click', function (e) {
   e.preventDefault();
   links.forEach(i => i.classList.toggle('linksTgl'));
   directorio.classList.toggle('directorioTgl');
-  //estela.classList.toggle('estelaTgl');
-  estealMain.classList.toggle('event');
-  estealMain.classList.toggle('estala-main-tgl')
-  
-  const event = document.querySelector('.event');
-  event.addEventListener('click', function (e) {
-    e.preventDefault();
-    links.forEach(i => i.classList.toggle('linksTgl'));
-    directorio.classList.toggle('directorioTgl');
-    //estela.classList.toggle('estelaTgl');
-    estealMain.classList.toggle('estala-main-tgl')
-  })
-  
+  estela.classList.toggle('estelaTgl');
 })
- */
+
+event.addEventListener('click', function (e) {
+  e.preventDefault();
+  links.forEach(i => i.classList.toggle('linksTgl'));
+  directorio.classList.toggle('directorioTgl');
+  estela.classList.toggle('estelaTgl');
+})
