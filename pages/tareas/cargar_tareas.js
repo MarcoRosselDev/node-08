657db450f7ad8cc772f844c0ffaa476afef8163a
Marco Rossel <andresmarcorossel@gmail.com>
2023-11-29 02:06:57 -0300

    tarea | refact 01

---
+++ pages/tareas/cargar_tareas.js
@@ -0,0 +1,47 @@
+import { cargar_estela } from "./cargar-estela.js";
+const main = document.querySelector('main');
+
+export const cargar_tareas = async (jwt, id_usuario) =>{
+  try {
+    const respuesta = await fetch(`/api/tarea/${id_usuario}`, {
+      method: 'GET',
+      headers: {
+        'Authorization': `Bearer ${jwt}`,
+        'Content-Type': 'application/json'
+      }
+    })
+
+    if (respuesta.status === 200) {
+      const promesa = respuesta.json();
+      promesa.then(objeto_tareas =>{
+        // iteramos por el objeto con las tareas lo agregamos al dom
+        const div = document.createElement('div');
+        div.classList.add('lista-tareas');
+
+        // para no manipular el dom en cada iteracion
+        let elemento_final = '';
+        objeto_tareas.map(iteracion =>{
+          elemento_final += `
+          <div class="tarea-individual">
+            <p>${iteracion.contenido}</p>
+            <!-- user id -->
+            <p class="ocultar">${iteracion.user_id}</p>
+            <!-- id de la tarea -->
+            <p class="ocultar">${iteracion._id}</p>
+            <div class="botones-edicion">
+              <button class="btn eliminar">eliminar</button>
+              <button class="btn editar">editar</button>
+            </div>
+          </div>`;
+        })
+        div.innerHTML = elemento_final;
+        main.append(div);
+        cargar_estela();
+      })
+    } else {
+      console.log('fetch GET fallido, respuesta:', respuesta);
+    }
+  } catch (error) {
+    console.log('error en cargar_tareas fn:', error);
+  }
+}
---
+++ pages/tareas/jwt_usuario.js
@@ -0,0 +1,33 @@
+import { cargar_tareas } from "./cargar_tareas.js";
+const main = document.querySelector('main')
+
+export const jwt_user = async () =>{
+  try {
+    const respuesta = await fetch('/api/getJwtCookie', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    if (respuesta.status === 200) {
+      const promesa = respuesta.json();
+      promesa.then(infoCookie =>{
+      // invocar la fn logout ---------------------------
+      main.innerHTML = `
+      <div class="estela evento"></div>
+      <div class="contenedor ">
+        <h2>Hola ${infoCookie.nombre}</h2>
+        <p>Escribe una nueva tarea</p>
+        <input type="text" class="input-nueva-tarea" id="input-tarea">
+        <button id="guardar" class="btn-guardar-nueva-tarea">guardar</button>
+      </div>
+      <div class="mensajes-fetch">
+      </div>`;
+      cargar_tareas(infoCookie.cookie.jwt, infoCookie.id)
+
+      })
+    }
+  } catch (error) {
+    console.log('error en la fn jwt_user:', error);
+  }
+}
