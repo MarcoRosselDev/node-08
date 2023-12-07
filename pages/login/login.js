bdfa1f0890f93856130171ba72d9a3ae259b695b
Marco Rossel <andresmarcorossel@gmail.com>
2023-12-07 10:34:18 -0300

    render url | 01

--- pages/login/login.js
+++ pages/login/login.js
@@ -3,6 +3,8 @@ const links = document.querySelectorAll('.links');
 const directorio = document.querySelector('.directorio');
 const estela = document.querySelector('.estela');
 const event = document.querySelector('.event');
+require('dotenv').config();
+const URL_PRODUCTION = process.env.URL_PRODUCTION;
 
 toggleBtn.addEventListener('click', function (e) {
   e.preventDefault();
@@ -93,7 +95,7 @@ login.addEventListener('click', async function (e) {
             })
           })
           // este es para trabajo local 
-          if (cookie.status === 200) return window.location.href = `http://localhost:3000/tareas/`;
+          if (cookie.status === 200) return window.location.href = `${URL_PRODUCTION}/tareas/`;
           // return window.location.href = `https://node-08-portfolio.onrender.com/tareas/`;
         } catch (error) {
           console.log('error en crear-cookie', error);
