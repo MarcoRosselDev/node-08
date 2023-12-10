155acbb5d4f95e6d3d806b62a5a494c18802e7f2
Marco Rossel <andresmarcorossel@gmail.com>
2023-12-10 06:13:23 -0300

    home | cv fn

--- public/home.js
+++ public/home.js
@@ -3,7 +3,7 @@ const links = document.querySelectorAll('.links');
 const directorio = document.querySelector('.directorio');
 const estela = document.querySelector('.estela');
 const event = document.querySelector('.event');
-
+const cv = document.querySelector('.cv');
 
 toggleBtn.addEventListener('click', function (e) {
   e.preventDefault();
@@ -19,4 +19,9 @@ event.addEventListener('click', function (e) {
   links.forEach(i => i.classList.toggle('linksTgl'));
   directorio.classList.toggle('directorioTgl');
   estela.classList.toggle('estelaTgl');
+})
+
+cv.addEventListener('click', function (e) {
+  e.preventDefault();
+  console.log('cv clicked, change the dom main');
 })
