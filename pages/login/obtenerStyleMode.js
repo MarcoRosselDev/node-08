08d90f01bbf0576bd118e2300aa5511b25364b55
Marco Rossel <andresmarcorossel@gmail.com>
2023-12-11 03:59:01 -0300

    home | header lenguajes 3

--- pages/login/obtenerStyleMode.js
+++ pages/login/obtenerStyleMode.js
@@ -15,7 +15,7 @@ const obtenerStyleMode = async () =>{
     if (response.status === 200) {
     const a = response.json();
     a.then(data => {
-      head.children[3].href = `https://marcorossel.com/login/login-${data.mode}.css`;
+      head.children[3].href = `login-${data.mode}.css`;
       div.classList.add(data.mode);
       if (data.mode === 'light') {
         modeBtn.innerText = 'dark mode';
@@ -27,13 +27,13 @@ const obtenerStyleMode = async () =>{
         e.preventDefault();
         if (div.classList.contains('light') === true) {
           div.classList.remove('light');
-          head.children[3].href = `https://marcorossel.com/login/login-dark.css`;
+          head.children[3].href = `login-dark.css`;
           div.classList.add('dark');
           actualizarStyleMode('dark'); //<------------------------light or dark | aplicar toggle fn
           modeBtn.innerText = 'light mode';
         } else{
           div.classList.remove('dark');
-          head.children[3].href = `https://marcorossel.com/login/login-light.css`;
+          head.children[3].href = `login-light.css`;
           div.classList.add('light');
           actualizarStyleMode('light');
           modeBtn.innerText = 'dark mode';
